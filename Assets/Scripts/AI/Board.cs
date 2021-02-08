be4b643b6ac3f0c7de9105f2d483f78ea8d18a08
George056 <70665330+George056@users.noreply.github.com>
2021-02-07 19:17:12 -0600

    Update Board.cs
    Found error in interpreting a node char to a tile and node position. Fixed by adding LocationSpliterNode function for the first tile that has a node. Began working on a function that finds every tile and node position with OtherTilesWithSameNode.
    A few function names added, but not implemented.
    Some usefull constants added (readonly).

--- Assets/Scripts/AI/Board.cs
+++ Assets/Scripts/AI/Board.cs
@@ -13,28 +13,40 @@ Branches as counted from the top most one and go from left to right and top to b
 
 public class Board : MonoBehaviour
 {
-    /*List<Node> nodes = new List<Node>(24);
-    List<Branch> branches = new List<Branch>(36);
-    List<short> max_nodes = new List<short>(13);
-
-    Board(List<Color> cl, List<short> max_value)
-    {
-
-    }*/
-
+    readonly List<char> OUTER_NODES = new List<char>() { 'a', 'b', 'c', 'f', 'g', 'l', 'm', 'r', 's', 'v', 'w', 'x' };
+    readonly List<int> OUTER_SIDES = new List<int>() { 0, 1, 2, 3, 5, 6, 9, 10, 14, 15, 20, 21, 25, 26, 29, 30, 32, 33, 34, 35 };
 
     List<Tile> __tiles;
 
+    public void MakeRandBoard()
+    {
+
+    }
+
+    public void MakeGivenBoard()
+    {
+
+    }
+
+    /// <summary>
+    /// This function checks to see if it is posible to place a node there
+    /// </summary>
+    /// <param name="location">The location to place the node</param>
+    /// <returns>If it is legal to place a node (true) or not (false)</returns>
     public bool LegalMoveNode(char location)
     {
-        int _tile_ = (int)location / 4;
-        int _node_ = (int)location % 4;
+        LocationSpliterNode(location, out int _tile_, out int _node_);
 
         Owner owner =__tiles[_tile_].CheckNodeColor(_node_);
 
         return owner == Owner.Nil;
     }
 
+    /// <summary>
+    /// This function checks to see if it is posible to place a connector there
+    /// </summary>
+    /// <param name="location">The location to place the connector</param>
+    /// <returns>If it is legal to place a connector (true) or not (false)</returns>
     public bool LegalMoveConnector(int location)
     {
         int _tile_ = location / 4;
@@ -43,5 +55,122 @@ public class Board : MonoBehaviour
         Owner owner = __tiles[_tile_].CheckConnectorColor(_side_);
 
         return owner == Owner.Nil;
+    }
+
+    public void PlaceNode(Owner who, char location)
+    {
+        LocationSpliterNode(location, out int _tile_, out int _node_);
+
+        //the edge nodes
+        if (OUTER_NODES.Contains(location))
+        {
+            __tiles[_tile_].CaptureNode(who, (short)_node_);
+        }//**************************************************************************************************************************************
+        //***************************************************************************************************************************************
+        //***************************************************************************************************************************************
+        //***************************************************************************************************************************************
+    }
+
+    public void PlaceConnector(int location)
+    {
+
+    }
+
+    /// <summary>
+    /// This finds the first occurrence of a node
+    /// </summary>
+    /// <param name="location">The location of the node</param>
+    /// <param name="_tile_">The tile the node is on (out)</param>
+    /// <param name="_node_">The node of the tile it is on (out)</param>
+    private void LocationSpliterNode(char location, out int _tile_, out int _node_)
+    {
+        if(location == 'a' || location == 'b' || location == 'd' || location == 'e')
+        {
+            _tile_ = 0;
+            _node_ = (location == 'a') ? 0 : (location == 'b') ? 1 : (location == 'd') ? 2 : 3;
+        }
+        else if(location == 'c' || location == 'h' || location == 'i')
+        {
+            _tile_ = 1;
+            _node_ = (location == 'c') ? 0 : (location == 'h') ? 2 : 3;
+        }
+        else if(location == 'j')
+        {
+            _tile_ = 2;
+            _node_ = 3;
+        }
+        else if(location == 'f' || location == 'k')
+        {
+            _tile_ = 3;
+            _node_ = (location == 'f') ? 1 : 3;
+        }
+        else if(location == 'g' || location == 'm' || location == 'n')
+        {
+            _tile_ = 4;
+            _node_ = (location == 'g') ? 0 : (location == 'm') ? 2 : 3;
+        }
+        else if(location == 'o')
+        {
+            _tile_ = 5;
+            _node_ = 3;
+        }
+        else if(location == 'p')
+        {
+            _tile_ = 6;
+            _node_ = 3;
+        }
+        else if(location == 'q')
+        {
+            _tile_ = 7;
+            _node_ = 3;
+        }
+        else if(location == 'l' || location == 'r')
+        {
+            _tile_ = 8;
+            _node_ = (location == 'l') ? 1 : 3;
+        }
+        else if(location == 's' || location == 't')
+        {
+            _tile_ = 9;
+            _node_ = (location == 's') ? 2 : 3;
+        }
+        else if(location == 'u')
+        {
+            _tile_ = 10;
+            _node_ = 3;
+        }
+        else if(location == 'v')
+        {
+            _tile_ = 11;
+            _node_ = 3;
+        }
+        else // location == 'w' || location == 'x'
+        {
+            _tile_ = 12;
+            _node_ = (location == 'w') ? 2 : 3;
+        }
+    }
+
+    private void OtherTilesWithSameNode(char location, int tile, int node, out List<int> tiles, out List<int> node_pos)
+    {
+        if(OUTER_NODES.Contains(location))//no other locations
+        {
+            tiles = new List<int>();
+            node_pos = new List<int>();
+        }
+        else
+        {
+            if(location == 'i' || location == 'j' || location == 'o' || location == 'p')//part of 4 tiles
+            {
+                tiles = new List<int>(3);
+                node_pos = new List<int>(3);
+            }
+            else//part of 3 tiles
+            {
+                tiles = new List<int>(2);
+                node_pos = new List<int>(2);
+            }
+        }
+
     }
 }

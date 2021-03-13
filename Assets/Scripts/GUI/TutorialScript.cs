bffa25363616ee491f0c90065a717360f0fbf4e7
George Cook <gcook1@harding.edu>
2021-03-13 14:51:14 -0600

    More tutorial work

--- Assets/Scripts/GUI/TutorialScript.cs
+++ Assets/Scripts/GUI/TutorialScript.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class TutorialScript : MonoBehaviour
 {
+    public GameObject[] TutorialSlides;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame

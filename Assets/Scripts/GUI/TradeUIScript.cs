fba38aa1dbb86d7eb5697c5da47f7b1498885f0b
marykyukich <45930127+marykyukich@users.noreply.github.com>
2021-03-13 23:45:15 -0600

    More work on Trade UI and enforcing trade rules

--- Assets/Scripts/GUI/TradeUIScript.cs
+++ Assets/Scripts/GUI/TradeUIScript.cs
@@ -11,9 +11,13 @@ public class TradeUIScript : MonoBehaviour
     public TextMeshProUGUI loyalistsTraded;
     public TextMeshProUGUI coinsTraded;
 
+    private int totalTradeCount = 0;
+
     public GameObject tradeWindow;
 
     List<int> tradeList = new List<int>(4) { 0, 0, 0, 0 };
+
+    //List<int> playerResources = GameObject.FindObjectOfType<Player>().__resources;
 
     public void toggleTradeWindow()
     {
@@ -23,14 +27,16 @@ public class TradeUIScript : MonoBehaviour
     public void selectCopperOut()
     {
         int numCopperTraded = Convert.ToInt32(copperTraded.text);
-        if (numCopperTraded == 3)
+        if (totalTradeCount == 3 || numCopperTraded == GameObject.FindObjectOfType<Player>().__resources[0])
         {
+            totalTradeCount -= numCopperTraded;
             numCopperTraded = 0;
             copperTraded.text = numCopperTraded.ToString();
             tradeList[0] = 0;
         }
         else
         {
+            totalTradeCount++;
             numCopperTraded++;
             copperTraded.text = numCopperTraded.ToString();
             tradeList[0]--;
@@ -40,14 +46,16 @@ public class TradeUIScript : MonoBehaviour
     public void selectLumberOut()
     {
         int numLumberTraded = Convert.ToInt32(lumberTraded.text);
-        if (numLumberTraded == 3)
+        if (totalTradeCount == 3 || numLumberTraded == GameObject.FindObjectOfType<Player>().__resources[1])
         {
+            totalTradeCount -= numLumberTraded;
             numLumberTraded = 0;
             lumberTraded.text = numLumberTraded.ToString();
             tradeList[1] = 0;
         }
         else
         {
+            totalTradeCount++;
             numLumberTraded++;
             lumberTraded.text = numLumberTraded.ToString();
             tradeList[1]--;
@@ -57,14 +65,16 @@ public class TradeUIScript : MonoBehaviour
     public void selectLoyalistOut()
     {
         int numLoyalistsTraded = Convert.ToInt32(loyalistsTraded.text);
-        if (numLoyalistsTraded == 3)
+        if (totalTradeCount == 3 || numLoyalistsTraded == GameObject.FindObjectOfType<Player>().__resources[2])
         {
+            totalTradeCount -= numLoyalistsTraded;
             numLoyalistsTraded = 0;
             loyalistsTraded.text = numLoyalistsTraded.ToString();
             tradeList[2] = 0;
         }
         else
         {
+            totalTradeCount++;
             numLoyalistsTraded++;
             loyalistsTraded.text = numLoyalistsTraded.ToString();
             tradeList[2]--;
@@ -74,14 +84,16 @@ public class TradeUIScript : MonoBehaviour
     public void selectCoinOut()
     {
         int numCoinsTraded = Convert.ToInt32(coinsTraded.text);
-        if (numCoinsTraded == 3)
+        if (totalTradeCount == 3 || numCoinsTraded == GameObject.FindObjectOfType<Player>().__resources[3])
         {
+            totalTradeCount -= numCoinsTraded;
             numCoinsTraded = 0;
             coinsTraded.text = numCoinsTraded.ToString();
             tradeList[3] = 0;
         }
         else
         {
+            totalTradeCount++;
             numCoinsTraded++;
             coinsTraded.text = numCoinsTraded.ToString();
             tradeList[3]--;
@@ -122,5 +134,7 @@ public class TradeUIScript : MonoBehaviour
         tradeList[1] = 0;
         tradeList[2] = 0;
         tradeList[3] = 0;
+
+        totalTradeCount = 0;
     }
 }

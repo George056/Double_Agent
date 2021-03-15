e2f7f47444dffdc8e5657a7f405d5bedc7d64a71
George Cook <gcook1@harding.edu>
2021-03-15 15:47:51 -0500

    Tutorial Framework Finished

--- Assets/Scripts/GUI/TutorialScript.cs
+++ Assets/Scripts/GUI/TutorialScript.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class TutorialScript : MonoBehaviour
 {
-    public GameObject[] TutorialSlides;
+    public GameObject[] TutorialSlides = new GameObject[2];
+    public Button BackButton;
+    public Button NextButton;
+    int slideNum = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +27,36 @@ public class TutorialScript : MonoBehaviour
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void TutorialBack()
+    {
+        TutorialSlides[slideNum].SetActive(false);
+        slideNum--;
+        if(slideNum == 0) //Hard code in array bounds
+        {
+            NextButton.interactable = true;
+        }
+        if(slideNum == 0)
+        {
+            BackButton.interactable = false;
+        }
+        TutorialSlides[slideNum].SetActive(true);
+
+    }
+
+    public void TutorialNext()
+    {
+        TutorialSlides[slideNum].SetActive(false);
+        if(slideNum == 0)
+        {
+            BackButton.interactable = true;
+        }
+        slideNum++;
+        if(slideNum == 1) //Hard code in array bounds
+        {
+            NextButton.interactable = false;
+        }
+        TutorialSlides[slideNum].SetActive(true);
     }
 }

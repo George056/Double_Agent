dcda40ebda51f6418266ef3d5d1dfddb9255d258
George Cook <gcook1@harding.edu>
2021-03-17 15:16:01 -0500

    Settings Framework

--- Assets/Scripts/GUI/SettingsScript.cs
+++ Assets/Scripts/GUI/SettingsScript.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
     public GameObject SettingsWindow;
 
+    public Slider MusicSlider;
+
+    public AudioSource Music;
+
+
     public void SettingsWindowPopUp()
     {
         SettingsWindow.SetActive(true);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        SettingsWindow.SetActive(false);
+    }
+
+    private void Update()
+    {
+        Music.volume = MusicSlider.value;
     }
 }

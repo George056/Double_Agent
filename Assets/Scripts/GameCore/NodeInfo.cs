eae856e6f33662deda73ab3a2115a773731c462d
marykyukich <45930127+marykyukich@users.noreply.github.com>
2021-03-16 19:33:22 -0500

    Starting on allowing un-placement of nodes

--- Assets/Scripts/GameCore/NodeInfo.cs
+++ Assets/Scripts/GameCore/NodeInfo.cs
@@ -8,6 +8,8 @@ public class NodeInfo : MonoBehaviour
     public Owner nodeOwner;
     public int nodeOrder;
 
+    public bool placementConfirmed = false;
+
     // List of resources the node gives
     public List<GameObject> resources = new List<GameObject>(0);
     
@@ -27,5 +29,33 @@ public class NodeInfo : MonoBehaviour
                 GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().PayForNode();
             }
         }
+        else if (nodeOwner == GameObject.FindObjectOfType<BoardManager>().activeSide && !placementConfirmed)
+        {
+            // change nodeOwner to Nil
+            // change color back to grey
+            // remove nodeOrder from player's list of owned nodes
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().__owned_nodes.Remove(nodeOrder);
+
+            // give player two coins and two loyalists back
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().__resources[2] += 2;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().__resources[3] += 2;
+        }
     }
+
+    //void OnMouseEnter()
+    //{
+    //    if (GameObject.FindObjectOfType<BoardManager>().inBuildMode && GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().CanAffordNode())
+    //    {
+    //        if (GameObject.FindObjectOfType<BoardManager>().LegalNodeMove(nodeOrder, GameObject.FindObjectOfType<BoardManager>().activeSide,
+    //            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().__owned_branches))
+    //        {
+    //            this.GetComponent<SpriteRenderer>().color = new UnityEngine.Color(0, 0, 150);
+    //        }
+    //    }
+    //}
+
+    //void OnMouseExit()
+    //{
+        
+    //}
 }

4ab4cef3aff29b2105e62e592ad0bb6075fc4d48
marykyukich <45930127+marykyukich@users.noreply.github.com>
2021-03-18 21:05:24 -0500

    Volume control works for all music tracks across both scenes

--- Assets/Scripts/GameCore/BoardManager.cs
+++ Assets/Scripts/GameCore/BoardManager.cs
@@ -110,6 +110,7 @@ public class BoardManager : MonoBehaviour
     public GameObject USMusic;
     public GameObject USSRMusic;
     public float defaultVolume = 0.5f;
+    public Slider musicSlider;
 
     private string customBoardSeed;
     public GameObject[] tempResourceList = new GameObject[13];
@@ -294,6 +295,7 @@ public class BoardManager : MonoBehaviour
 
         USMusic.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume", defaultVolume);
         USSRMusic.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume", defaultVolume);
+        //musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", defaultVolume);
 
         end = false;
         turnCount = 1;

5748260849fff4d04bfaaff87f8019fea7d4747d
George Cook <gcook1@harding.edu>
2021-03-26 09:22:43 -0500

    End Game Sound Effects Set up

--- Assets/Scripts/GUI/SettingsScript.cs
+++ Assets/Scripts/GUI/SettingsScript.cs
@@ -8,13 +8,19 @@ public class SettingsScript : MonoBehaviour
     public GameObject SettingsWindow;
 
     public Slider MusicSlider;
+    public Slider SoundEffectSlider;
 
     public AudioSource Music1;
     public AudioSource Music2;
+    public AudioSource SFX1;
+    public AudioSource SFX2;
+    public AudioSource SFX3;
+    public AudioSource SFX4;
 
     void Awake()
     {
         MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        SoundEffectSlider.value = PlayerPrefs.GetFloat("SoundEffectsVolume", 0.5f);
     }
 
     public void SettingsWindowPopUp()
@@ -32,5 +38,14 @@ public class SettingsScript : MonoBehaviour
         Music1.volume = MusicSlider.value;
         Music2.volume = MusicSlider.value;
         PlayerPrefs.SetFloat("MusicVolume", MusicSlider.value);
+    }
+
+    public void ChangeSoundEffectVolume()
+    {
+        SFX1.volume = SoundEffectSlider.value;
+        SFX2.volume = SoundEffectSlider.value;
+        SFX3.volume = SoundEffectSlider.value;
+        SFX4.volume = SoundEffectSlider.value;
+        PlayerPrefs.SetFloat("SoundEffectsVolume", SoundEffectSlider.value);
     }
 }
